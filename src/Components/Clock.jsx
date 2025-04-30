00aca7ff239455fecf544542e85feb424f7eb053
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-04-30 14:00:44 +0530

    2nd commit

---
+++ src/Components/Clock.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Clock.css'
+
+const Clock = () => {
+  return (
+<div className="page-wrapper">
+  <div className="clock-container">
+    <h1>Hi Vineeth</h1>
+  </div>
+</div>
+  )
+}
+
+export default Clock
