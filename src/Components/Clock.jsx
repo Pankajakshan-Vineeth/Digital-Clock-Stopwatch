4301869dfc098db0706be1d960c34b4ada063f70
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-04-30 14:18:06 +0530

    added clock function

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Clock.css'
 
 const Clock = () => {
+
+    let getTime = new Date().toLocaleTimeString();
+
+    const [currentTime, setCurrentTime] = useState(getTime);
+
+    let runningTime = ()=>{
+        getTime = new Date().toLocaleTimeString();
+        setCurrentTime(getTime);
+    }
+
+    setInterval(runningTime,1000)
+   
+
   return (
 <div className="page-wrapper">
   <div className="clock-container">
-    <h1>Hi Vineeth</h1>
+    <h1>{currentTime}</h1>
   </div>
 </div>
   )
